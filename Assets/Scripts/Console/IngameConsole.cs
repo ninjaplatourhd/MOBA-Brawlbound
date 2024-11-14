9eb057846ac891f75c0b6b0b26306a142fd0ad5d
ninjaplatourhd <99766521+ninjaplatourhd@users.noreply.github.com>
2024-11-14 19:38:50 +0100

    Dodao automatsko logovanje u konzolu

--- Assets/Scripts/Console/IngameConsole.cs
+++ Assets/Scripts/Console/IngameConsole.cs
@@ -1,16 +1,13 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
-using UnityEngine.UIElements;
-using System.Threading.Tasks;
 using System.IO;
-using System;
-using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class IngameConsole : MonoBehaviour {
+public class IngameConsole : MonoBehaviour
+{
 
     public string LocalClientName { get => client; set { client = value; } }
 
@@ -26,50 +23,78 @@ public class IngameConsole : MonoBehaviour {
     private List<string> messages = new List<string>();
     private string client = "Anon";
 
-    public void Start() {
+    public void Start()
+    {
         AppendSystemMessage("Started Game...");
     }
+    private void OnEnable()
+    {
+        // Register to listen to log messages
+        Application.logMessageReceived += HandleLog;
+    }
 
-    public void Update() {
-        if (inputField.text != "" && Input.GetKeyUp(KeyCode.Return)) {
+    private void OnDisable()
+    {
+        // Unregister when the object is disabled
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    private void HandleLog(string logString, string stackTrace, LogType type)
+    {
+
+        string formattedMessage = $"[{type}] {logString}";
+        AppendSystemMessage(formattedMessage);
+    }
+
+    public void Update()
+    {
+        if (inputField.text != "" && Input.GetKeyUp(KeyCode.Return))
+        {
             AppendMessage(client, inputField.text);
-            inputField.text = "";         
+            inputField.text = "";
         }
     }
 
-    public void AppendMessage(string sender, string msg) {
+    public void AppendMessage(string sender, string msg)
+    {
         messages.Add("[" + sender + "]: " + msg + "\n");
         RefreshChat();
     }
 
-    public void AppendSystemMessage(string msg) {
+    public void AppendSystemMessage(string msg)
+    {
         messages.Add("[System]: " + msg + "\n");
         RefreshChat();
     }
 
-    public void ClearChat() {
+    public void ClearChat()
+    {
         messages.Clear();
         RefreshChat();
     }
 
-    public void RemoveMessage(int index) {
+    public void RemoveMessage(int index)
+    {
         messages.RemoveAt(index);
         RefreshChat();
     }
 
-    public void LogChatHistoryToFile() {
+    public void LogChatHistoryToFile()
+    {
 
         string filePath = Path.Combine("Logs/IngameLogs/", "ingame-log-" + DateTime.Now.ToString().Replace("/", "-").Replace(" ", "-").Replace(":", "-") + ".txt");
 
         Directory.CreateDirectory("Logs/IngameLogs");
 
-        using (StreamWriter writer = new(filePath)) {
-            foreach(string msg in messages)
+        using (StreamWriter writer = new(filePath))
+        {
+            foreach (string msg in messages)
                 writer.Write(msg);
         }
     }
 
-    public async void RefreshChat() {
+    public async void RefreshChat()
+    {
         content.text = "";
 
         foreach (string msg in messages)
