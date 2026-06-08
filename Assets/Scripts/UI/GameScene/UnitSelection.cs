7bd889691b4a8584002bf75a14a3e074370359f5
Savo Smiljić <savosmiljic1998@gmail.com>
2026-06-08 13:21:53 +0200

    Popravio unit select

--- Assets/Scripts/UI/GameScene/UnitSelection.cs
+++ Assets/Scripts/UI/GameScene/UnitSelection.cs
@@ -12,6 +12,9 @@ public class UnitSelection : MonoBehaviour
     private Vector2 _startingPoint;
     private RectTransform rectTransform;
 
+    private bool _isDragging;
+    private float _dragThreshold = 10f;
+
     private Camera _camera;
     private void Awake()
     {
@@ -22,37 +25,111 @@ public class UnitSelection : MonoBehaviour
     {
         _camera = Camera.main;
     }
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        _selectionArea.gameObject.SetActive(true);
+    //        _startingPoint = Input.mousePosition;
+
+    //        rectTransform.position = _startingPoint;
+
+    //        rectTransform.sizeDelta = Vector2.zero;
+    //    }
+    //    else if (Input.GetMouseButton(0))
+    //    {
+    //        Vector2 mousePosition = Input.mousePosition;
+    //        Vector2 size = new Vector2(
+    //         Mathf.Abs(mousePosition.x - _startingPoint.x),
+    //         Mathf.Abs(mousePosition.y - _startingPoint.y)
+    //     );
+
+    //        rectTransform.pivot = new Vector2(
+    //            mousePosition.x < _startingPoint.x ? 1 : 0,
+    //            mousePosition.y < _startingPoint.y ? 1 : 0
+    //        );
+
+    //        rectTransform.sizeDelta = size;
+    //    }
+    //    if (Input.GetMouseButtonUp(0))
+    //    {
+    //        SelectUnitsInRectangle();
+    //        _selectionArea.SetActive(false);
+    //    }
+    //}
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            _selectionArea.gameObject.SetActive(true);
+            _isDragging = false;
             _startingPoint = Input.mousePosition;
 
+            _selectionArea.SetActive(true);
             rectTransform.position = _startingPoint;
-
             rectTransform.sizeDelta = Vector2.zero;
         }
+
         else if (Input.GetMouseButton(0))
         {
             Vector2 mousePosition = Input.mousePosition;
-            Vector2 size = new Vector2(
-             Mathf.Abs(mousePosition.x - _startingPoint.x),
-             Mathf.Abs(mousePosition.y - _startingPoint.y)
-         );
 
-            rectTransform.pivot = new Vector2(
-                mousePosition.x < _startingPoint.x ? 1 : 0,
-                mousePosition.y < _startingPoint.y ? 1 : 0
-            );
+            if (!_isDragging)
+            {
+                if (Vector2.Distance(mousePosition, _startingPoint) > _dragThreshold)
+                {
+                    _isDragging = true;
+                }
+            }
 
-            rectTransform.sizeDelta = size;
+            if (_isDragging)
+            {
+                Vector2 size = new Vector2(
+                    Mathf.Abs(mousePosition.x - _startingPoint.x),
+                    Mathf.Abs(mousePosition.y - _startingPoint.y)
+                );
+
+                rectTransform.pivot = new Vector2(
+                    mousePosition.x < _startingPoint.x ? 1 : 0,
+                    mousePosition.y < _startingPoint.y ? 1 : 0
+                );
+
+                rectTransform.sizeDelta = size;
+            }
         }
-        if (Input.GetMouseButtonUp(0))
+
+        else if (Input.GetMouseButtonUp(0))
         {
-            SelectUnitsInRectangle();
             _selectionArea.SetActive(false);
+
+            if (_isDragging)
+            {
+                SelectUnitsInRectangle();
+            }
+            else
+            {
+                SelectSingleUnit();
+            }
         }
+    }
+
+    private void SelectSingleUnit()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, LayerMask.GetMask("Clickable")))
+        {
+            if (hit.collider.TryGetComponent<Unit>(out Unit unit))
+            {
+                if (unit.BelongsToLocalPlayer())
+                {
+                    UnitManager.instance.SelectUnit(unit.gameObject);
+                    return;
+                }
+            }
+        }
+
+        UnitManager.instance.DeSelectAll();
     }
 
     private void SelectUnitsInRectangle()
