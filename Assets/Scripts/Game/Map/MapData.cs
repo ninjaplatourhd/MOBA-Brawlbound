f1ebcecfc5c2aad9be937262f2c57f4f408ef7a7
ninjaplatourhd <99766521+ninjaplatourhd@users.noreply.github.com>
2026-06-16 16:03:56 +0200

    Promenjen novi tenk sa boljim modelom

---
+++ Assets/Scripts/Game/Map/MapData.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData
+{
+
+    [SerializeField] public List<GameObject> StartingBaseLocations;
+    [SerializeField] public Vector2 MapSize;
+}
--- Assets/Scripts/Game/Map/MapOption.cs
+++ Assets/Scripts/Game/Map/MapOption.cs
@@ -7,4 +7,5 @@ public class MapOption
     public string DisplayName;
     public string SceneName;
     public Sprite PreviewImage;
+    public int MaxNumOfPlayers = 2;
 }
